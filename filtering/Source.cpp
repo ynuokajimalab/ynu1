2b60ef508cdd1ba4823fa3a2e65dcc73c5418717
傑 佐藤 <sgr0416st@gmail.com>
2016-09-14 13:14:30 +0900

    電車の音を抽出

--- filtering/Source.cpp
+++ filtering/Source.cpp
@@ -16,15 +16,15 @@ int main(void)
 	int n, m, k, J, L, N, offset, frame, number_of_frame;
 	double fe, delta, *b, *w, *b_real, *b_imag, *x_real, *x_imag, *y_real, *y_imag;
 
-	mono_wave_read(&pcm0, "sample04.wav"); /* WAVEファイルからモノラルの音データを入力する */
+	mono_wave_read(&pcm0, "005_160615_0941V0.wav"); /* WAVEファイルからモノラルの音データを入力する */
 
 	pcm1.fs = pcm0.fs; /* 標本化周波数 */
 	pcm1.bits = pcm0.bits; /* 量子化精度 */
 	pcm1.length = pcm0.length; /* 音データの長さ */
 	pcm1.s = (double*)calloc(pcm1.length, sizeof(double)); /* メモリの確保 */
 
-	fe = 1000.0 / pcm0.fs; /* エッジ周波数 */
-	delta = 1000.0 / pcm0.fs; /* 遷移帯域幅 */
+	fe = 600.0 / pcm0.fs; /* エッジ周波数 */
+	delta = 500.0 / pcm0.fs; /* 遷移帯域幅 */
 
 	J = (int)(3.1 / delta + 0.5) - 1; /* 遅延器の数 */
 	if (J % 2 == 1)
@@ -97,7 +97,7 @@ int main(void)
 		}
 	}
 
-	mono_wave_write(&pcm1, "ex6_5.wav"); /* WAVEファイルにモノラルの音データを出力する */
+	mono_wave_write(&pcm1, "ex6_4.wav"); /* WAVEファイルにモノラルの音データを出力する */
 
 	free(pcm0.s); /* メモリの解放 */
 	free(pcm1.s); /* メモリの解放 */
